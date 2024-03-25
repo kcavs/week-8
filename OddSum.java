7c94ad063fee67c328d3fb4855503ee152095c6c
kcavs <kcav1321@gmail.com>
2024-03-25 14:58:11 -0400

    done

---
+++ OddSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class OddSum {
+    public static void main(String[]args){
+        int num;
+        int sum=0;
+        Scanner input=new Scanner(System.in);
+        System.out.print("enter a integer: ");
+        num= input.nextInt();
+        for(int goes=num;num>0;goes--){
+            int test=num%2;
+            if(test==1){
+                System.out.print(num+" ");
+            }
+            num-=1;
+        }
+    }
+}
